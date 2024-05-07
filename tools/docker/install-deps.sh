3a18a862ca5a9c868763ce33662906b52a47f998
stocco <stoccotommaso02@gmail.com>
2024-05-07 13:04:15 +0200

    messo tools

---
+++ tools/docker/install-deps.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get -qq update
+
+sudo apt-get -qq install -y --no-install-recommends \
+	aspell \
+	aspell-en \
+	aspell-it \
+	lmodern \
+	latexmk \
+	poppler-utils \
+	texlive \
+	texlive-extra-utils \
+	texlive-lang-english \
+	texlive-lang-italian \
+	texlive-latex-extra \
+	texlive-latex-recommended \
+	texlive-luatex \
+	texlive-pictures\
+	texlive-fonts-extra\
